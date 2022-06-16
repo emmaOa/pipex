1b6327ef91e1d35802ab3b678e44c602397b5e66
Imane Ouazzani chahdi <iouazzan@e1r13p2.1337.ma>
2022-06-16 16:11:25 +0100

    close fd_pipe

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -6,38 +6,55 @@
 /*   By: iouazzan <iouazzan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 02:51:01 by iouazzan          #+#    #+#             */
-/*   Updated: 2022/06/14 19:51:04 by iouazzan         ###   ########.fr       */
+/*   Updated: 2022/06/16 16:10:46 by iouazzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 #include <stdio.h>
 
-char	*ft_url_bonus(char *path, t_pipe *pp)
+char		*ft_url_bonus(char *path, t_pipe *pp)
 {
-	int		i;
-	char	**url;
-	char	*cmd_path;
+	int i;
+	char **tmp;
 
 	i = 0;
-	url = ft_split(path, ':');
-	while (pp->param[i] != NULL)
+	tmp = ft_split(path, ':');
+	// for (int m = 0 ; m < 5; m++){
+	// 	printf("%s\n", tmp[m]);
+	// }
+	while (tmp[i])
 	{
-		// printf("%s\n", pp->param[i]);
+		tmp[i] = ft_strjoin(tmp[i], "/");
+		tmp[i] = ft_strjoin(tmp[i], pp->param[0]);
+		if (access(tmp[i], F_OK) == 0)
+			return(tmp[i]);
 		i++;
 	}
-	// printf("hiii\n");
-	while (url[i])
-	{
-		cmd_path = ft_strjoin(url[i], "/");
-		cmd_path = ft_strjoin(cmd_path, pp->param[0]);
-		if (access(cmd_path, F_OK) == 0)
-			return (cmd_path);
-		i++;
-	}
-	// printf("%s\n", cmd_path);
 	return (NULL);
 }
+
+// char	*ft_url_bonus(char *path, t_pipe *pp)
+// {
+// 	int		i;
+// 	char	**url;
+// 	char	*cmd_path;
+
+// 	i = 0;
+// 	url = ft_split(path, ':');
+
+// 	while (pp->param[i] != NULL)
+// 		i++;
+// 	while (url[i])
+// 	{
+// 		cmd_path = ft_strjoin(url[i], "/");
+// 		cmd_path = ft_strjoin(cmd_path, pp->param[0]);
+// 		if (access(cmd_path, F_OK) == 0)
+// 			return (cmd_path);
+// 		i++;
+// 	}
+// 	return (NULL);
+// }
 
 char	*ft_path_bonus(char *env[])
 {
@@ -65,104 +82,65 @@ char	*ft_path_bonus(char *env[])
 	return (tmp);
 }
 
-void	ft_param_bonus(char *arv[], t_pipe *pp, int arc)
-{
-
-	// int i;
-	// int x;
-	// int r = 0;
-	// int nb = 0;
-
-	// i = 2;
-	// x = 0;
-	// while (arv[pp->i][r])
-	// {
-	// 	if (arv[pp->i][r] == ' ' && arv[pp->i][r + 1] != ' ')
-	// 		nb++;
-	// 	r++;
-	// }
-	
-	// pp->param = (char **)malloc(nb + 1 * sizeof(char *));
-	// while (x < nb + 1 )
-	// {
-    //    pp->param[x] = (char *)malloc((strlen(arv[i]) + 1) * sizeof(char));
-	// 	x++;
-	// 	i++;
-	// }
-	pp->param = ft_split(arv[arc - 4 + pp->i], ' ');
-	// i = 2;
-	// x = 0;
-	// while (i < arc - 1)
-	// {
-
-	// 	pp->param[x] = ft_strdup(arv[i]);
-	// 	i++;
-	// 	x++;
-	// }
-
-}
-
-// void	ft_param_bonus(char *arv[], t_pipe *pp)
+// void	ft_param_bonus(char *arv[], t_pipe *pp, int arc)
 // {
-// 	pp->param = ft_split(arv[pp->i], ' ');
+
+// 	// int i;
+// 	// int x;
+// 	// int r = 0;
+// 	// int nb = 0;
+
+// 	// i = 2;
+// 	// x = 0;
+
+// 	// while (arv[pp->i][r])
+// 	// {
+// 	// 	if (arv[pp->i][r] == ' ' && arv[pp->i][r + 1] != ' ')
+// 	// 	{
+// 	// 		printf("check\n");
+// 	// 		printf("space %d\n", nb);
+// 	// 		nb++;
+// 	// 	}
+// 	// 	r++;
+// 	// }
+		
+
+// 	// printf("%d\n", nb);
+// 	// pp->param = (char **)malloc(nb + 1 * sizeof(char *));
+// 	// while (x < nb + 1 )
+// 	// {
+//     //    pp->param[x] = (char *)malloc((strlen(arv[i]) + 1) * sizeof(char));
+// 	// 	x++;
+// 	// 	i++;
+// 	// }
+// 	pp->param = ft_split(arv[arc - 4 + pp->i], ' ');
+// 	// for (int l = 0; l < 2; l++)
+// 	// {
+// 	// 	printf("%s  \n", pp->param[l]);
+
+// 	// }
+// 	// exit(1);
+// 	// i = 2;
+// 	// x = 0;
+// 	// while (i < arc - 1)
+// 	// {
+
+// 	// 	pp->param[x] = ft_strdup(arv[i]);
+// 	// 	i++;
+// 	// 	x++;
+// 	// }
+
 // }
 
 
-// int main(int arc, char *arv[], char *env[])
-// {
-// 	t_pipe	pp;
-// 	int i;
-// 	int	j;
-
-// 	i = 3;
-// 	j = 1;
-// 	pp.nb_cmnd = arc - 3;
-// 	pp.nb_pipe = pp.nb_cmnd - 1;
-// 	pp.name_file = arv[1];
-// 	pp.fd_file = open(pp.name_file, O_RDONLY, 0644);
-// 	if (pp.fd_file == -1)
-// 		ft_exit_bonus();
-// 	ft_fd_pipe(&pp);
-// 	pp.fr1 = fork();
-// 	if (pp.fr1 == -1)
-// 		ft_exit_bonus();
-// 	if (pp.fr1 == 0)
-// 		ft_fork_1_bonus(&pp, arv, env);
-// 	while (pp.nb_cmnd - 2 > 0)
-// 	{
-// 		pp.fr1 = fork();
-// 		if (pp.fr1 == -1)
-// 			ft_exit_bonus();
-// 		if (pp.fr1 == 0)
-// 		ft_cmnd(&pp, arv[arc - i], env);
-// 		dup2(pp.fd_pipe[j - 1][0], 0);
-// 		dup2(pp.fd_pipe[j][1], 1);
-// 		close(pp.fd_pipe[j][1]);
-// 		close(pp.fd_pipe[j][0]);
-// 		i++;
-// 		// j++;
-// 		pp.nb_cmnd--;
-// 		if (execve(pp.url, pp.param, env) < 0)
-// 		ft_exit_bonus();
-// 	}
-// 	pp.fr1 = fork();
-// 	if (pp.fr1 == -1)
-// 		ft_exit_bonus();
-// 	if (pp.fr1 == 0)
-// 		ft_fork_last_bonus(&pp, arv[arc - 1], env);
-// 	return (0);
-// }
 
 int main(int arc, char *arv[], char *env[])
 {
 	t_pipe	pp;
+	int j;
  	int		**fd_pipe;
 	int		x;
-
-	// int		fd_pipe[arc - 1][2];
-
-	 
-	 
+ 
 	x = 0;
 	(void)env;
 	pp.i = 0;
@@ -190,7 +168,7 @@ int main(int arc, char *arv[], char *env[])
 		pp.i++;
 	}
 	pp.i = 0;
-	dup2(pp.fd_file, 0);
+	
 	while (pp.i < pp.nb_fork)
 	{
 		pp.fr[pp.i] = fork();
@@ -208,17 +186,30 @@ int main(int arc, char *arv[], char *env[])
 				pp.j++;
 			}
 			ft_fork_bonus(&pp, arv, env, fd_pipe, arc);
-			break;
+			printf("jj\n");
+			return (0);
 		}
 		pp.i++;
 	}
-	pp.i = 0;
-	while (pp.i < pp.nb_fork)
+	j = 0;
+	while (j < pp.nb_pipe)
+	{
+		if (j != pp.nb_fork)
+			close(fd_pipe[j][0]);
+		if (j != 0)
+			close(fd_pipe[j][1]);
+		j++;
+	}
+	dup2(pp.fd_file, 0);
+	dup2(pp.fd_file_2, fd_pipe[pp.i][1]);
+	close(fd_pipe[pp.nb_fork][0]);
+	close(fd_pipe[0][1]);
+	j = 0;
+	while (j < pp.nb_fork)
 	{
 		wait(NULL);
-		pp.i++;
+		j++;
 	}
-	dup2(pp.fd_file_2, fd_pipe[pp.i][1]);
-	close(fd_pipe[0][0]);
-	close(fd_pipe[pp.i][1]);
+	return (0);
+
 }
--- pipex_utl_bonus.c
+++ pipex_utl_bonus.c
@@ -6,7 +6,7 @@
 /*   By: iouazzan <iouazzan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 21:03:13 by iouazzan          #+#    #+#             */
-/*   Updated: 2022/06/14 02:51:56 by iouazzan         ###   ########.fr       */
+/*   Updated: 2022/06/16 16:09:47 by iouazzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_exit_bonus(void)
 	exit (1);
 }
 
-
+// void	ft_close(t_pipe *pp, )
 
 // void	ft_cmnd(t_pipe *pp, char arv[], char *env[])
 // {
@@ -31,51 +31,14 @@ int	ft_exit_bonus(void)
 
 void	ft_fork_bonus(t_pipe *pp, char *arv[], char *env[], int *fd_pipe[], int arc)
 {
-	
-	ft_param_bonus(arv, pp, arc);
-	// printf("%d\n", pp->i);
+	pp->param = ft_split(arv[arc - 4 + pp->i], ' ');
 	pp->url = ft_url_bonus(ft_path_bonus(env), pp);
-	exit(1);
 	if (pp->url == NULL)
 		ft_exit_bonus();
 	dup2(fd_pipe[pp->i + 1][1], 1);
 	dup2(fd_pipe[pp->i][0], 0);
 	close(fd_pipe[pp->i][0]);
 	close(fd_pipe[pp->i + 1][1]);
-	printf("-----\n");
 	if (execve(pp->url, pp->param, env) < 0)
 		ft_exit_bonus();
 }
-
-// void	ft_fork_last_bonus(t_pipe *pp, char arv[], char *env[])
-// {
-// 	pp->param = ft_param_bonus(arv);
-// 	pp->url = ft_url_bonus(ft_path_bonus(env), pp->param[0]);
-// 	if (pp->url == NULL)
-// 		ft_exit_bonus();
-// 	pp->fd_last_file = open(arv, O_CREAT | O_RDWR, 0644);
-// 	if (pp->fd_last_file == -1)
-// 		ft_exit_bonus();
-// 	dup2(pp->fd_last_file, 1);
-// 	dup2(ppfd_pipe[pp->nb_pipe - 1][0], 0);
-// 	close(pp->fd_pipe[pp->nb_pipe - 1][1]);
-// 	close(pp->fd_pipe[pp->nb_pipe - 1][0]);
-// 	if (execve(pp->url, pp->param, env) < 0)
-// 		ft_exit_bonus();
-// }
-
-// t_pipe		*ft_fd_pipe(t_pipe *pp)
-// {
-// 	int i;
-
-// 	i = 0;
-// 	pp->fd_pipe = malloc(pp->nb_pipe * sizeof(int));
-// 	while (i < pp->nb_pipe)
-// 	{
-// 		pp->fd_pipe[i] = malloc(2 * sizeof(int));
-// 		if (pipe(pp->fd_pipe[i]) == -1)
-// 			ft_exit_bonus();
-// 		i++;
-// 	}
-// 	return (pp);
-// }
